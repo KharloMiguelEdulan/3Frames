1f3c14ba43c2f0655a0ab9ff1630288d35588964
KharloMiguelEdulan <edulan.kharlo2001@gmail.com>
2024-11-05 13:57:02 +0800

    new

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Recovery from './Components/Recover';
 
-export default function App() {
+
+import backgroundImage from './Components/gg.png';
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('login');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'login':
+        return <Login onSwitch={() => setCurrentPage('recovery')} onRegister={() => setCurrentPage('register')} />;
+      case 'register':
+        return <Register onSwitch={() => setCurrentPage('login')} />;
+      case 'recovery':
+        return <Recovery onSwitch={() => setCurrentPage('login')} />;
+      default:
+        return <Login onSwitch={() => setCurrentPage('recovery')} onRegister={() => setCurrentPage('register')} />;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <ImageBackground source={backgroundImage} style={styles.background}>
+        <View>
+          {renderPage()}
+        </View>
+      </ImageBackground>
+    </PaperProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
+    alignItems: 'center', 
   },
 });
+
+export default App;
